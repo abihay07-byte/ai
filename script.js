cab06a2997426583718d4518a8cf648de11480c8
abihay07-byte <abihay07@gmail.com>
2026-07-30 10:21:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,13 +20,17 @@ async function sendMessage() {
   chatBox.appendChild(loadingElem);
 
   try {
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        contents: [{ parts: [{ text: text }] }]
-      })
-    });
+   const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    contents: [{ parts: [{ text: text }] }]
+  })
+});      
+      
+      
+              
+    
 
     const data = await response.json();
 
